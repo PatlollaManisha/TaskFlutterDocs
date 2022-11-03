ca56d29e0873c644b62a3eb64a9a9aea4d8dfedb
PatlollaManisha <115684773+PatlollaManisha@users.noreply.github.com>
2022-11-03 11:45:15 +0530

    API

--- lib/login_api_call.dart
+++ lib/login_api_call.dart
@@ -21,60 +21,63 @@ class _LoginAPIcallsState extends State<LoginAPIcalls> {
         centerTitle: true,
       ),
       body: Form(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-                width: 100, height: 100, child: Lottie.asset("lottie.json")),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                width: 326,
-                height: 50,
-                child: TextFormField(
-                  controller: _phoneNumberController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.phone_iphone_outlined),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  width: 100, height: 100, child: Lottie.asset("lottie.json")),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  width: 326,
+                  height: 50,
+                  child: TextFormField(
+                    controller: _phoneNumberController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.phone_iphone_outlined),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              child: SizedBox(
-                width: 326,
-                height: 50,
-                child: TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.password),
-                    suffixIcon: Icon(Icons.remove_red_eye),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: SizedBox(
+                  width: 326,
+                  height: 50,
+                  child: TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password),
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                width: 326,
-                height: 50,
-                child: ElevatedButton(
-                    onPressed: () {
-                      //if (_formKey.currentState!.validate()) {}
-                      requestApi(context);
-                    },
-                    child: Text(
-                      "LogIn",
-                      style: TextStyle(fontSize: 26),
-                    )),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  width: 326,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        //if (_formKey.currentState!.validate()) {}
+                        requestApi(context);
+                      },
+                      child: Text(
+                        "LogIn",
+                        style: TextStyle(fontSize: 26),
+                      )),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -83,13 +86,37 @@ class _LoginAPIcallsState extends State<LoginAPIcalls> {
   void requestApi(BuildContext context) async {
     final requestUrl = "http://164.52.213.195:8088/salex/" + "authenticate";
     final requestPayload = {
-      "password": _phoneNumberController.text, //"Ak@123"
-      "phoneNumber": _passwordController.text //7205807205
+      "password": _phoneNumberController.text.toString(), //"Ak@123"
+      "phoneNumber": _passwordController.text.toString() //7205807205
     };
     final _dioObject = Dio();
     try {
       final response = await _dioObject.post(requestUrl, data: requestPayload);
       print(response);
-    } catch (e) {}
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 400 || e.response?.statusCode == 500) {
+        final errorMessage = e.response?.data["message"];
+        showAlert(errorMessage);
+      }
+      print("error is $e");
+
+      print("status code is ${e.response?.statusCode}");
+    }
+  }
+
+  void showAlert(errorMessage) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(title: Text(errorMessage), actions: [
+            ElevatedButton(
+              onPressed: () {
+                // print("button Action");
+                Navigator.pop(context);
+              },
+              child: const Text("ok"),
+            )
+          ]);
+        });
   }
 }
